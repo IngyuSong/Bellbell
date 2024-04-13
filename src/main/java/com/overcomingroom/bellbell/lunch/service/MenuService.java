1311719b3243ba9babe59e7ee1210d4e3aa907f1
Ingyu Song <ingyu3879@gmail.com>
2024-04-13 14:18:38 +0900

    :bug: fix: 빌드 과정에서 생긴 문제 수정

--- src/main/java/com/overcomingroom/bellbell/lunch/service/MenuService.java
+++ src/main/java/com/overcomingroom/bellbell/lunch/service/MenuService.java
@@ -1,25 +1,22 @@
 package com.overcomingroom.bellbell.lunch.service;
 
-import com.overcomingroom.bellbell.exception.CustomException;
-import com.overcomingroom.bellbell.exception.ErrorCode;
 import com.overcomingroom.bellbell.lunch.domain.entity.Menu;
 import com.overcomingroom.bellbell.lunch.repository.MenuRepository;
 import jakarta.annotation.PostConstruct;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.io.UrlResource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.URI;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 @Component
 @RequiredArgsConstructor
@@ -28,18 +25,17 @@ import java.util.Random;
 public class MenuService {
 
     private final MenuRepository menuRepository;
+    private final EntityManager entityManager;
+
+    @Value("classpath:csv/menu.csv")
+    private Resource resource;
 
     @PostConstruct
     private void initMenu() {
 
-        // 파일 경로 읽기
-        String fileLocation = "csv/menu.csv";
-        Path path = Paths.get(fileLocation);
-        URI uri = path.toUri();
-
         List<Menu> menuList = new ArrayList<>();
 
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(new UrlResource(uri).getInputStream()))) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(resource.getInputStream()))) {
             String line = "";
 
             while ((line = br.readLine()) != null) {
@@ -57,19 +53,12 @@ public class MenuService {
 
     // 랜덤으로 메뉴를 선정함.
     public List<Menu> recommendMenus() {
+        TypedQuery<Menu> query = entityManager.createQuery(
+            "SELECT m FROM Menu m ORDER BY FUNCTION('RAND')", // JPQL 사용
+            Menu.class
+        );
 
-        // id의 처음 ~ 마지막 번호 까지 랜덤 정수 생성
-        Random random = new Random();
-        List<Menu> findAllMenu = menuRepository.findAll();
-        Long min = findAllMenu.get(0).getMenuId();
-        Long max = findAllMenu.get(findAllMenu.size() - 1).getMenuId();
-
-        // 메뉴 3가지 추천
-        List<Menu> menuList = new ArrayList<>();
-        for (int i = 1; i < 4; i++) {
-            Long randomNumber = random.nextLong(max - min + 1) + min;
-            menuList.add(menuRepository.findById(randomNumber).orElseThrow(() -> new CustomException(ErrorCode.FAILED_TO_GENERATE_RANDOM_NUMBER)));
-        }
-        return menuList;
+        query.setMaxResults(3); // 랜덤하게 선택할 메뉴 개수 지정
+        return query.getResultList();
     }
 }
